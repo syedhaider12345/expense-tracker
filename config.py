7d211cf8b80fb80b16dcd04f275c7f52ec42d142
Haider Abbas <haiderabbas@Haiders-MacBook-Pro.local>
2026-03-15 09:14:28 +0530

    Fix config to use GROQ_API_KEY

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY      = os.getenv("GROQ_API_KEY", "")
-EMAIL_SENDER      = os.getenv("EMAIL_SENDER", "")
+GROQ_API_KEY       = os.getenv("GROQ_API_KEY", "")
+EMAIL_SENDER       = os.getenv("EMAIL_SENDER", "")
 EMAIL_APP_PASSWORD = os.getenv("EMAIL_APP_PASSWORD", "")
-APP_URL           = os.getenv("APP_URL", "http://localhost:8000")
+APP_URL            = os.getenv("APP_URL", "http://localhost:8000")
